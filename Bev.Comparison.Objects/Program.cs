f0bd27032fa204701c0393e34a7e9e8615888df6
Michael Matus <michael@matus.at>
2023-06-11 21:54:48 +0200

    Create irrelevant source modification

--- Bev.Comparison.Objects/Program.cs
+++ Bev.Comparison.Objects/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
+using System.Configuration;
 using Bev.Comparison.Objects.Objects;
 using Bev.Comparison.Objects.Objects.Report;
 using Bev.Comparison.Objects.Objects.Report.Artefact;
